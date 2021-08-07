990e1ae9122572e2d8f6f1e6e0b207209b053f06
Terence Broad <terry.m.broad@gmail.com>
2021-08-07 15:10:44 +0100

    basic training working

--- model.py
+++ model.py
@@ -1,37 +1,34 @@
 import torch
+import math
 from torch import nn
 from torch.nn import functional as F
 from torch.autograd import Function
 
 def get_active_func(s):
     if s == "ReLU":
-        return nn.ReLU
+        return nn.ReLU()
     elif s == "LeakyReLU":
-        return nn.LeakyReLU
-    eluf s == "Tanh":
-    return nn.Tanh
+        return nn.LeakyReLU()
+    elif s == "Tanh":
+        return nn.Tanh()
     else:
-        print("activation function {}, is not known, defaulting to LeakyReLU.".format{s})
-        return nn.LeakyReLU
+        print("activation function %s, is not known, defaulting to LeakyReLU."%(s))
+        return nn.LeakyReLU()
     
 
-Class Encoder(nn.Module):
+class Encoder(nn.Module):
     def __init__(
         self,
-        size,
         latent_dim
     ):
         super().__init__()
-        self.size = size
         self.latent_dim = latent_dim
         self.convs = nn.ModuleList()
-        self.fc = nn.Linear(9216, self.latent_dim)
+        self.fc = nn.Linear(6656, self.latent_dim)
         
-        if hidden_dims is None:
-            hidden_dims = [64, 128, 256, 512, 512]
+        hidden_dims = [64, 128, 256, 512, 512]
 
         in_channels = 1
-        # Build Encoder
         for h_dim in hidden_dims:
             self.convs.append(
                 ConvLayer(in_channels,h_dim,5,2,activation_function="ReLU") 
@@ -44,13 +41,13 @@ Class Encoder(nn.Module):
         return mu + eps*std
 
     def kl_divergence(self, mu, logvar):
-        return (-0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())) / self.code_dim
+        return (-0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())) / self.latent_dim
 
     def encode(self, input):
         x = input
-        for conv in enumerate(self.convs):
+        for conv in self.convs:
             x = conv(x)
-        out = x.squeeze(2).squeeze(2)
+        out = torch.flatten(x,start_dim=1)
         out = self.fc(out)
         return out, out
 
@@ -61,37 +58,40 @@ Class Encoder(nn.Module):
         return z, kld
 
 
-Class Decoder(nn.Module):
+class Decoder(nn.Module):
     def __init__(
         self,
-        size,
         latent_dim
     ):
         super().__init__()
-        self.size = size
+        hidden_dims = [512, 256, 128, 64]
         self.latent_dim = latent_dim
-        self.decoder_input = nn.Linear(latent_dim, hidden_dims[0]*4)
+        self.decoder_input = nn.Linear(latent_dim, hidden_dims[0]*13)
         self.convs = nn.ModuleList()
-        
-        if hidden_dims is None:
-            hidden_dims = [512, 512, 256, 128, 64]
-
-        in_channels = 1
-        # Build Encoder
+        in_channels = hidden_dims[0]
         for h_dim in hidden_dims:
-            self.convs.append(
-                ConvLayer(in_channels,h_dim,5,2,activation_function="ReLU",transpose=True) 
-            )
+            if h_dim < 256:
+                self.convs.append(
+                    ConvLayer(in_channels,h_dim,5,2,output_padding=(1),activation_function="ReLU",transpose=True) 
+                )
+            else:
+                self.convs.append(
+                    ConvLayer(in_channels,h_dim,5,2,activation_function="ReLU",transpose=True) 
+                )
             in_channels = h_dim
-        self.final_layer = ConvLayer(hidden_dims[-1], 1, 5, 2, activation_function="Tanh",transpose=True) 
+        self.final_layer = ConvLayer(64, 1, 3, 2, activation_function="Tanh",transpose=True) 
 
     def forward(self, input):
         x = self.decoder_input(input)
-        for conv in enumerate(self.convs):
+        x = torch.reshape(x, (64,512,1,13))
+        for conv in self.convs:
             x = conv(x)
         x = self.final_layer(x)
+        #drop row from tensor bc I cannit figure out how to get them the same dim
+        x = x[:,:,0:128,:] 
+        return x
 
-Class ConvLayer(nn.Module):
+class ConvLayer(nn.Module):
     def __init__(
         self,
         in_channel, 
@@ -99,44 +99,52 @@ Class ConvLayer(nn.Module):
         kernel_size, 
         stride=1, 
         padding=0, 
+        output_padding=0,
         bias=True,
         activation_function="ReLU",
         transpose=False
     ):
         super().__init__()
 
-        self.size = size
-        self.activation_function = get_activ_function(activation_function)
+        self.activation_function = get_active_func(activation_function)
         self.weight = nn.Parameter(
             torch.randn(out_channel, in_channel, kernel_size, kernel_size)
         )
         self.scale = 1 / math.sqrt(in_channel * kernel_size ** 2)
         self.transpose = transpose
+        self.stride = stride
+        self.padding = padding
+        self.output_padding = output_padding
+        self.kernel_size = kernel_size
+        self.in_channel=in_channel
+        self.out_channel=out_channel
+        self.bias = bias
 
-        if bias:
-            self.bias = nn.Parameter(torch.zeros(out_channel))
-
+        if self.transpose == True:
+            self.layer_seq = nn.Sequential(
+                nn.ConvTranspose2d(
+                self.in_channel,
+                self.out_channel,
+                kernel_size=self.kernel_size,
+                bias=self.bias,
+                stride=self.stride,
+                padding=self.padding,
+                output_padding=self.output_padding), 
+                self.activation_function
+            )
         else:
-            self.bias = None
-
+            self.layer_seq = nn.Sequential(
+                nn.Conv2d(
+                self.in_channel,
+                self.out_channel,
+                kernel_size=self.kernel_size,
+                bias=self.bias,
+                stride=self.stride,
+                padding=self.padding),
+                self.activation_function
+            )
 
     def forward(self, input):
-        if self.transpose == True:
-            out = F.ConvTranspose2d(
-                input,
-                self.weight * self.scale,
-                bias=self.bias,
-                stride=self.stride,
-                padding=self.padding,
-            )
-        else:
-            out = F.conv2d(
-                input,
-                self.weight * self.scale,
-                bias=self.bias,
-                stride=self.stride,
-                padding=self.padding,
-            )
-        out = nn.BatchNorm2d(out)
-        return self.activation_function(out)
+        out = self.layer_seq(input)
+        return out
 
